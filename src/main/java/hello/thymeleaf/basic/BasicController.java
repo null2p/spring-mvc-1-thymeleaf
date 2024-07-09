35feae84f33492b65bdeeaf6ace1d13b3f0a9688
nuri park <nuri_park2@tmax.co.kr>
2024-07-09 21:33:54 +0900

    [feat] 조건문 예제 추가

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -104,13 +104,20 @@ public class BasicController {
         return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
     private void addUsers(Model model) {
+
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
         list.add(new User("userB", 20));
         list.add(new User("userC", 30));
 
-        model.addAttribute("users",list);
+        model.addAttribute("users", list);
     }
 
     @Data
